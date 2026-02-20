cb851c083d10ec10d60e61f287815f8ffa8b00c8
jeevajack30032000-cell <jeevajack30032000@gmail.com>
2026-02-20 11:16:16 +0530

    day 1 assignment

---
+++ Day 1 assignment/task 1&2.py
@@ -0,0 +1,20 @@
+'''
+1.ex
+--------------
+'''
+accountholdername = input("enter the accountholdername")
+age = int( input(" enter the age") )
+accountnumber = str( input("enter the account number") )
+print("customer name ",accountholdername,"Age",age,"ac.no",accountnumber)
+
+'''
+2.ex
+---------------
+'''
+math = int( input("enter the math mark") )
+python = int( input("enter the python mark") )
+java = int( input("enter the java mark") )
+react = int( input("enter the react mark") )
+django = int( input("enter the django mark") )
+average = math+python+java+react+django/5
+print("total average is",average)
