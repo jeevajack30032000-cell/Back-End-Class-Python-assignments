876e4e3f33262fdd8de739062d619466750584d6
jeevajack30032000-cell <jeevajack30032000@gmail.com>
2026-03-01 16:44:35 +0530

    day 6 assign

---
+++ Day 6 Assignment/Task 1&2.py
@@ -0,0 +1,36 @@
+##task 1:
+##     *
+##   * *
+## * * *
+
+
+for i in range(1,4):
+    for j in range((3-i)*2):
+        print(" ",end="")
+    for star in range(i):
+        print(" *",end="")
+    print()
+
+
+
+##task 2:
+##       *
+##    *  *
+## *  *  *
+##    *  *
+##       *
+
+
+for i in range(1,4):
+    for j in range((3-i)*2):
+        print(" ",end="")
+    for star in range(i):
+        print(" *",end="")
+    print()
+for i in range(2,0,-1):
+    for j in range((3-i)*2):
+        print(" ",end="")
+    for star in range(i):
+        print(" *",end="")
+    print()
+    
